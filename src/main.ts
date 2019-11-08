3a3738d8d4ecac84886668c7ab9417c903eeeeea
v-haoxiaolong.ea <v-haoxiaolong.ea@inc.bytedance.com>
2019-11-08 14:56:13 +0800

    添加测试组件

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import DefaultInput from './views/Home.vue'
+const components = {
+  DefaultInput
+}
+Vue.component('DefaultInput', DefaultInput)
 Vue.config.productionTip = false
 
 new Vue({
